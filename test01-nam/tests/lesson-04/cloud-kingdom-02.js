4264e771e78083a8033497420f431655f821e40a
test 01 - Nam <hoangnamqa99@gmail.com>
2026-01-12 15:48:58 +0700

    add solution for challenge 03

---
+++ test01-nam/tests/lesson-04/cloud-kingdom-02.js
@@ -0,0 +1,9 @@
+let powerUp = "mushroom";
+let effect = '';
+
+let message =
+    powerUp === "mushroom" ? ' Mario become Supper! ' :
+        powerUp === "flower" ? ' Mario can shoot fireballs! ;' :
+            powerUp === "star" ? ' Mario is invincible! ' :
+                ' Mario is small! ';
+console.log(message);
